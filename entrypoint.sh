ef512357f825bfe9afb6cfd9b3a21ea8e43cf6b8
vipin-discovery <122345527+vipin-discovery@users.noreply.github.com>
2023-01-12 11:45:16 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ else
     file="/github/workspace/$INPUT_LOCUSTFILE"
 fi
 
-chmod +x backend/docker/entrypoints/entrypoint.sh
+chmod +x /home/vipin/GHA-Locust-Load-Test/entrypoint.sh
 locust -f $file --headless -u $INPUT_USERS -r $INPUT_RATE --run-time $INPUT_RUNTIME -H $INPUT_URL
